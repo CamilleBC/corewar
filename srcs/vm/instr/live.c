a1e22ad2174c953e4e046cb07c7648cfa7c3e4e8
babariviere <babariviere@protonmail.com>
2018-03-21 16:50:24 +0100

    [vm] fix live

--- srcs/vm/instr/live.c
+++ srcs/vm/instr/live.c
@@ -6,7 +6,7 @@
 /*   By: cbaillat <cbaillat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/15 09:19:25 by briviere          #+#    #+#             */
-/*   Updated: 2018/03/21 15:09:24 by briviere         ###   ########.fr       */
+/*   Updated: 2018/03/21 16:48:57 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	instr_live(t_vm *vm, t_proc *proc)
 {
 	size_t		i;
 	uint32_t	id;
+	uint32_t	id_player;
 
 	proc->live++;
 	if (proc->instr.nb_args != 1)
@@ -24,7 +25,8 @@ void	instr_live(t_vm *vm, t_proc *proc)
 	i = 0;
 	while (i < vm->nb_players)
 	{
-		if (id == vm->players[i]->id)
+		id_player = -vm->players[i]->id - 1;
+		if (id == id_player)
 		{
 			vm->players[i]->live++;
 			break ;
