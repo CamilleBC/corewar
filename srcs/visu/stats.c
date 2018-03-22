daa1d5098705d4e2a8cc5b136d92fdf918f0c92d
babariviere <babariviere@protonmail.com>
2018-03-22 11:07:30 +0100

    [visu] small changes

--- srcs/visu/stats.c
+++ srcs/visu/stats.c
@@ -6,7 +6,7 @@
 /*   By: cbaillat <cbaillat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/16 14:35:47 by cbaillat          #+#    #+#             */
-/*   Updated: 2018/03/21 10:12:26 by cbaillat         ###   ########.fr       */
+/*   Updated: 2018/03/22 10:59:01 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,12 +49,12 @@ void	print_vm_stats(t_vm *vm)
 {
 	wattron(vm->wins.stats_win,COLOR_PAIR(WHITEP_BLACK));
 	wmove(vm->wins.stats_win, 1, 1);
-	wprintw(vm->wins.stats_win, "Cycles: %llu", vm->total_cycles);
+	wprintw(vm->wins.stats_win, "Cycle: %llu", vm->total_cycles);
 	wmove(vm->wins.stats_win, 2, 1);
 	if (vm->flags & (1 << DUMP))
 		wprintw(vm->wins.stats_win, "Dump: %llu", vm->dump);
 	wmove(vm->wins.stats_win, 3, 1);
-	wprintw(vm->wins.stats_win, "Cycles to die: %llu", vm->cycles_to_die);
+	wprintw(vm->wins.stats_win, "Cycles to die: %lld", vm->cycles_to_die);
 	wmove(vm->wins.stats_win, 4, 1);
 	wprintw(vm->wins.stats_win, "Cycle delta: %d", CYCLE_DELTA);
 	wmove(vm->wins.stats_win, 5, 1);
