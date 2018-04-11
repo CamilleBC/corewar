fcad1a963a21c8d334d0d11a2067e2eff944a7c9
Camille Baillat <camille.baillat@protonmail.com>
2018-04-11 17:25:14 +0200

    [visu] now displaying the winner when visual exits

--- srcs/vm/main.c
+++ srcs/vm/main.c
@@ -6,7 +6,7 @@
 /*   By: cbaillat <cbaillat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/12 19:50:22 by briviere          #+#    #+#             */
-/*   Updated: 2018/04/11 17:14:45 by cbaillat         ###   ########.fr       */
+/*   Updated: 2018/04/11 17:22:21 by cbaillat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,9 +40,9 @@ int				main(int ac, char **av)
 	if (vm.flags & (1 << VISUAL))
 		init_visu(&vm);
 	run_vm(&vm);
-	winner = get_player_by_id(vm.last_live_id, vm);
-	ft_print("le joueur %d(%s) a gagne\n", vm.last_live_id, winner->header.prog_name);
 	if (vm.flags & (1 << VISUAL))
 		free_visu(vm.wins, vm.nb_players);
+	winner = get_player_by_id(vm.last_live_id, vm);
+	ft_print("le joueur %d(%s) a gagne\n", vm.last_live_id, winner->header.prog_name);
 	return (0);
 }
