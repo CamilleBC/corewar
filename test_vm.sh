759949862bf644aa34eaa9eab6e5e9a556e0cb6b
briviere <briviere@student.42.fr>
2018-04-12 17:12:31 +0200

    [script] mv script vm + clear output

--- test_vm.sh
+++ test_vm.sh
@@ -2,13 +2,15 @@
 us_corewar=corewar
 zaz_corewar=resources/corewar
 
-for ((count=1; count < 10000; count += 10))
+for ((count=100; count < 10000; count += 100))
 do
 	./$us_corewar -d $count $1 $2 > us
 	./$zaz_corewar -d $count $1 $2 > zaz
 	sed '1,3d' zaz > tmp; mv tmp zaz
-	echo $count"-------------------------------------\n"
-	diff us zaz
-	echo "-------------------------------------------\n"
+	colordiff us zaz
+	if [ $? != 0 ]; then
+		echo "Error on cycle $count"
+		break
+	fi
 done
 rm zaz us
