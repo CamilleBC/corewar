70967405cd577815ae4821e3297151321eb388ce
briviere <briviere@student.42.fr>
2018-04-12 11:48:48 +0200

    [norme] mini norme

--- srcs/visu/header.c
+++ srcs/visu/header.c
@@ -6,7 +6,7 @@
 /*   By: cbaillat <cbaillat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/16 14:34:45 by cbaillat          #+#    #+#             */
-/*   Updated: 2018/04/11 10:27:51 by briviere         ###   ########.fr       */
+/*   Updated: 2018/04/12 11:48:23 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@ void	print_header(t_vm *vm)
 	int	x_offset;
 
 	x_offset = (HEADER_W / 2) - 22;
-	// wattron(vm->wins.header_win, A_BOLD);
-	// wattron(vm->wins.header_win, COLOR_PAIR(RED_BLACK));
 	wattron(vm->wins.header_win, COLOR_PAIR(WHITEP_BLACK));
 	wborder(vm->wins.header_win, '#', '#', '#', '#', '#', '#', '#', '#');
 	mvwprintw(vm->wins.header_win, 1, x_offset, HEADER_LINE_1);
@@ -28,7 +26,5 @@ void	print_header(t_vm *vm)
 	mvwprintw(vm->wins.header_win, 4, x_offset, HEADER_LINE_4);
 	mvwprintw(vm->wins.header_win, 5, x_offset, HEADER_LINE_5);
 	wattroff(vm->wins.header_win, COLOR_PAIR(WHITEP_BLACK));
-	// wattroff(vm->wins.header_win, COLOR_PAIR(RED_BLACK));
-	// wattroff(vm->wins.header_win, A_BOLD);
 	wrefresh(vm->wins.header_win);
 }
