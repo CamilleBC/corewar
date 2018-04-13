b3f32f4b7d709c1d0521102e749ad8202ae6b638
THO_MAS <thomasgunzburger@hotmail.fr>
2018-04-13 18:18:22 +0200

    encore des changements pour avoir le cycle precis et plus rapidement

--- test_vm.sh
+++ test_vm.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 us_corewar=corewar
 zaz_corewar=resources/corewar
+add=1000
 
-for ((count=3000; count < 10000; count++))
+for ((count=1; count < 10000; count += add))
 do
 	./$us_corewar -d $count $1 $2 > us
 	./$zaz_corewar -d $count $1 $2 > zaz
 	sed -n '/^0x0000/,$p' zaz > tmp; mv tmp zaz
 	diff us zaz
 	if [ $? != 0 ]; then
-		echo "Error on cycle $count"
-		break
+		if [ $add == 1 ]; then
+			break;
+		fi
+		count=$(($count - $add))
+		add=$(($add / 10))
 	fi
 	echo $count
 done
