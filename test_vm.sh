565fca874c7672e22e653d4040ab30c9663baf53
briviere <briviere@student.42.fr>
2018-04-16 11:21:32 +0200

    [script] script now find the first diff cycle

--- test_vm.sh
+++ test_vm.sh
@@ -2,21 +2,32 @@
 us_corewar=corewar
 zaz_corewar=resources/corewar
 add=1000
+sub=-10
+error=0
+to_end=0
 
+echo "comparing vms..."
 for ((count=1000; count < 10000; count += add))
 do
-	./$us_corewar -d $count $1 $2 > us
-	./$zaz_corewar -d $count $1 $2 > zaz
+	./$us_corewar -d $count $@ > us
+	./$zaz_corewar -d $count $@ > zaz
 	sed -n '/^0x0000/,$p' zaz > tmp; mv tmp zaz
-	colordiff zaz us
+	diff zaz us &> log
 	#wdiff --no-common zaz us
-	if [ $? != 0 ]; then
-		if [ $add == 1 ]; then
-			break;
+	if [[ $? != 0 ]]; then
+		if [[ $to_end == 1 ]]; then
+			# phase 3: we have found the smallest cycle
+			echo cycle: $count
+			cat log | colordiff
+			break
 		fi
-		count=$(($count - $add))
-		add=$(($add / 10))
+		# phase 1: we found error so we go backward
+		error=$(($error + 1))
+		add=$(($error * $sub))
+	elif [[ $error > 0 ]]; then
+		# phase 2: no we go
+		add=1
+		to_end=1
 	fi
-	echo $count
 done
-rm zaz us
+rm zaz us log
