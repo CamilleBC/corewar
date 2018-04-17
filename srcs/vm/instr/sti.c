8f1182c99e00e25a6ba3cbd1b7b09d93d6159ce5
THO_MAS <thomasgunzburger@hotmail.fr>
2018-04-17 11:41:38 +0200

    fix d'erreur avec le sti

--- srcs/vm/instr/sti.c
+++ srcs/vm/instr/sti.c
@@ -6,7 +6,7 @@
 /*   By: cbaillat <cbaillat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/16 13:58:02 by briviere          #+#    #+#             */
-/*   Updated: 2018/04/16 17:12:33 by briviere         ###   ########.fr       */
+/*   Updated: 2018/04/16 19:10:51 by tgunzbur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ static int8_t	read_val(t_vm *vm, t_proc *proc, const t_arg *arg,
 static int8_t	read_val2(t_proc *proc, const t_arg *arg, int32_t *res)
 {
 	if (arg->code == DIR_CODE)
-		*res += arg->value.dir % IDX_MOD;
+		*res += arg->value.dir;
 	else if (arg->code == REG_CODE)
 		*res += proc->regs[arg->value.reg - 1];
 	else
@@ -52,7 +52,10 @@ void			instr_sti(t_vm *vm, t_proc *proc)
 		return ;
 	if (read_val2(proc, instr.args + 2, &addr) == ERROR)
 		return ;
-	addr = (proc->pc + (addr % IDX_MOD)) % MEM_SIZE;
+	if (addr < 32768)
+		addr = (proc->pc + (addr % IDX_MOD)) % MEM_SIZE;
+	else
+		addr = (proc->pc + ((addr - 65536) % -IDX_MOD)) % MEM_SIZE;
 	write_arena((t_arena_args){vm->arena, addr, 4}, reg_val,
 			proc->owner->colour);
 }
